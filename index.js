4a0ef737e7ea56b5e520b2bacbedacfecef5a5e7
Nicholas <najernigin@gmail.com>
2023-09-14 17:44:38 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let name1= "";
+let name2= "";
+let name3= "";
+let newArray=[];
+
+function countDown(){
+    let num = 10;
+    while(num>=0){
+        console.log(num--);
+    }
+}
+const array = ["","",""];
+let suprise ="";
+
+
+function writeCards(array,suprise){
+   for(let i=0;i<=2;i++){
+     newArray.splice(2,0,`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+        return newArray;
+    
+}
